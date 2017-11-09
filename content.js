6f2ab575209bc592fd8c55f63802f6d315921366
ryanlondon <ryel590@gmail.com>
2017-11-09 14:01:56 -0800

    it works

--- content.js
+++ content.js
@@ -2,12 +2,7 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
 
     const words = request.data;
-    const p = document.getElementsByTagName('p')[0];
-    p.textContent = words[0];
-      console.log(sender.tab ?
-              "from a content script:" + sender.tab.url :
-              "from the extension: " + words);
 
-      if (request.data == "hello")
-          sendResponse({farewell: "goodbye"});
+    $('body').find(words[0]).css({ backgroundColor: words[1] });
+
 });
