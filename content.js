360bb8b322ec8cde41d342c4b6ca812fe3b7e125
ryanlondon <ryel590@gmail.com>
2017-11-09 10:59:57 -0800

    changes first paragraph

--- content.js
+++ content.js
@@ -1,8 +1,12 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+
+    const words = request.data;
+    const p = document.getElementsByTagName('p')[0];
+    p.textContent = words[0];
       console.log(sender.tab ?
               "from a content script:" + sender.tab.url :
-              "from the extension: " + request.data);
+              "from the extension: " + words);
 
       if (request.data == "hello")
           sendResponse({farewell: "goodbye"});
